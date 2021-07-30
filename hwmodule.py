9786a94dc8c707686f8782277076d27fbabfe627
guynew28 <guynew28@yahoo.com>
2021-07-30 16:27:13 -0400

    hwmodule

---
+++ hwmodule.py
@@ -0,0 +1,19 @@
+
+pi=3.14
+
+def sqfthouse():
+    length=input('What is the length of the house?')
+    width=input('What is the width of the house')
+    return int(length)*int(width)
+print(sqfthouse())
+
+
+
+def circumference():
+    radius=input('What is the radius of the circle?')
+    return int(radius)*2*pi
+print(circumference())
+
+
+
+
